fe6632e0a17736718ca2989ceb555a70b72c966c
Lucy Petrova <164482879+lucypetrova22@users.noreply.github.com>
2025-03-13 22:14:55 +0200

    Create Contact.java

---
+++ Contact.java
@@ -0,0 +1,21 @@
+public class Contact {
+    private int id;
+    private String name;
+    private String phoneNumber;
+    private String email;
+    private String address;
+
+    public Contact(int id, String name, String phoneNumber, String email, String address) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getPhoneNumber() { return phoneNumber; }
+    public String getEmail() { return email; }
+    public String getAddress() { return address; }
+}
