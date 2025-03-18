8d1c369324f982f96f419eb871231eff15d5bd16
Lucy Petrova <164482879+lucypetrova22@users.noreply.github.com>
2025-03-19 02:05:06 +0300

    Update connection.java

--- connection.java
+++ connection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class connection {
     private static final String URL = "jdbc:mysql://localhost:3306/PhonebookDB";
     private static final String USER = "root";  //
-    private static final String PASSWORD = "Tiger064310.";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
